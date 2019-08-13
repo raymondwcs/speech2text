ee31b83279531562df0eb8c19cf57fa873375659
Raymond SO <rso@ouhk.edu.hk>
2019-08-13 20:39:42 +0800

    initial version

---
+++ README.md
@@ -0,0 +1,12 @@
+# COMPS381F - IBM Speech to Text Cloud Service[Reference]
+
+1. Go to the page below to learn more about IBM Cloud Speech to Text Cloud Service
+```
+https://cloud.ibm.com/docs/services/speech-to-text?topic=speech-to-text-gettingStarted
+```
+
+2. Follow the instructions in the **Before you begin** section of **Getting started tutorial** to obtain an `api-key`.
+
+3. Follow the instructions in the **Step 1: Transcribe audio with no options** section to transcribe a FLAC audio file.
+
+4. Replace the `APIKEY` in `server.js` with the your own key (step 2).
---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const app = express();
+const fetch = require('node-fetch');
+const APIKEY = '';
+
+/*
+curl -X POST -u "apikey:{apikey}" \
+--header "Content-Type: audio/flac" \
+--data-binary @{path_to_file}audio-file.flac \
+"{url}/v1/recognize"
+*/
+
+/* curl -u "apikey:{apikey" */
+var base64encodedData = Buffer.from(`apikey:${APIKEY}`).toString('base64');
+
+var options = {
+    host: 'gateway-tok.watsonplatform.net',
+    port: 80,
+    path: 'speech-to-text/api/v1/recognize',
+    method: 'POST',
+    headers: {
+    	 'Content-Type': 'audio/flac', 
+       'Authorization': `Basic ${base64encodedData}`
+    },
+    json: true 
+};
+
+app.set('view engine', 'ejs');
+
+// middlewares
+app.use(fileUpload());
+app.use(bodyParser.json());
+
+app.get('/',function(req,res) {
+  res.render('form',{});
+})
+
+app.post('/upload', function(req, res) {
+  if (!req.files) {
+      res.status(500).end('No files were uploaded.');
+  }
+
+  options['body'] = req.files.audio_file.data;
+
+  speech2Text().then(data => {
+	res.json(data);
+  })
+})
+
+async function speech2Text() {
+   const res = await fetch(`https://${options.host}/${options.path}`,options);
+   const data = await res.json();
+   
+   return(data);
+}
+
+app.listen(process.env.PORT || 8099, function() {
+    console.log('Server running...');
+});
